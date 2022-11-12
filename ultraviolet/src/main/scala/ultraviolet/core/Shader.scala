0dbc49ba22df69b4c0877a74bab670bfc7c19950
Dave Smith <davesmith.00000@gmail.com>
2022-11-12 21:35:53 +0000

    Convert Shader to a class

--- ultraviolet/src/main/scala/ultraviolet/core/Shader.scala
+++ ultraviolet/src/main/scala/ultraviolet/core/Shader.scala
@@ -1,27 +1,14 @@
 package ultraviolet.core
 
-import scala.annotation.targetName
 import scala.deriving.Mirror
 
-/** Out `Shader` is a program that can be run on a graphics card as part of the rendering pipeline. Indigo supports two
-  * kinds of shaders: Vertex and Fragment
-  *
-  * The Shader type is similar to a Reader monad (and so exposes `ask` and `asks`) that has some of the types fixed. The
-  * idea is that it provides the shader programs environment context, for example it allows access to a `UV` field for
-  * working with texture coordinates.
+/** A `Shader` is a program that can be run on a graphics card as part of the rendering pipeline.
   */
-opaque type Shader[In, Out] = In => Out
+final class Shader[In, Out](body: In => Out)
 object Shader:
-  inline def apply[In](f: In => Unit): Shader[In, Unit]    = f
-  inline def apply[In, Out](f: In => Out): Shader[In, Out] = f
-  inline def apply(body: => Any): Shader[Unit, Unit]       = (_: Unit) => body
-  // inline def apply[In, Out](value: Out): Shader[In, Out]    = _ => value
-  // inline def pure[In, Out](value: Out): Shader[In, Out]     = _ => value
-  // inline def fixed[In, Out](value: Out): Shader[In, Out]    = _ => value
-  // inline def ask[In]: Shader[In, In]                 = identity
-
-  // inline def join[In, B](ctx: Shader[In, Shader[In, B]]): Shader[In, B] =
-  //   (env: In) => ctx(env).run(env)
+  inline def apply[In, Out](f: In => Out): Shader[In, Out] = new Shader[In, Out](f)
+  inline def apply[In](f: In => Unit): Shader[In, Unit]    = new Shader[In, Unit](f)
+  inline def apply(body: => Any): Shader[Unit, Unit]       = new Shader[Unit, Unit]((_: Unit) => body)
 
   extension (inline ctx: Shader[Unit, Unit])
     inline def toGLSL: String =
@@ -30,16 +17,3 @@ object Shader:
   extension [In, Out](inline ctx: Shader[In, Out])
     inline def toGLSL(using Mirror.ProductOf[In]): String =
       ShaderMacros.toAST(ctx).toGLSL[In]
-//   inline def apply(env: In): Out                                                   = run(env)
-//   inline def map[B](f: Out => B): Shader[In, B]                                    = (e: In) => f(ctx(e))
-//   inline def ap[B](f: Shader[In, Out => B]): Shader[In, B]                        = (e: In) => map(f.run(e))(e)
-//   inline def flatten[B](using ev: Out <:< Shader[In, B]): Shader[In, B]           = join((env: In) => ev(run(env)))
-//   inline def flatMap[B](f: Out => Shader[In, B]): Shader[In, B]                   = join(map(f))
-//   inline def ask: Shader[In, In]                                                = identity
-//   inline def asks(f: In => Out): Shader[In, Out]                                    = (e: In) => f(e)
-//   inline def run(env: In): Out                                                     = ctx(env)
-//   inline def |*|[B](other: Shader[In, B]): Shader[In, (Out, B)]                   = combine(other)
-//   inline def combine[B](other: Shader[In, B]): Shader[In, (Out, B)]               = (e: In) => (run(e), other.run(e))
-//   inline def merge[B, C](other: Shader[In, B])(f: ((Out, B)) => C): Shader[In, C] = combine(other).map(f)
-//   inline def |>[B](ff: Pipeline[In, Out, B]): Shader[In, B]                       = pipe(ff)
-//   inline def pipe[B](ff: Pipeline[In, Out, B]): Shader[In, B]                     = ff.runWith(ctx)
