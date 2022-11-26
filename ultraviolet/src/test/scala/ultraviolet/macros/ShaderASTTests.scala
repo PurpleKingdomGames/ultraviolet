d2345af4bf55680e5e374018b837f0f89960698a
Dave Smith <davesmith.00000@gmail.com>
2022-11-26 22:26:04 +0000

    Added a failing test for struct support

--- ultraviolet/src/test/scala/ultraviolet/macros/ShaderASTTests.scala
+++ ultraviolet/src/test/scala/ultraviolet/macros/ShaderASTTests.scala
@@ -1479,6 +1479,51 @@ class ShaderASTTests extends munit.FunSuite {
     )
   }
 
+  test("structs can be declared and used") {
+
+    inline def fragment =
+      Shader[Unit, Unit] { _ =>
+        class Light(
+            val eyePosOrDir: vec3,
+            val isDirectional: Boolean,
+            val intensity: vec3,
+            val attenuation: Float
+        )
+
+        def makeLight(): Light =
+          Light(vec3(1.0f), true, vec3(2.0f), 2.5f)
+
+        def frag: Unit =
+          val x = makeLight()
+          val y = x.eyePosOrDir.y
+      }
+
+    val actual =
+      fragment.toGLSL[WebGL2].code
+
+    DebugAST.toAST(fragment)
+    // println(actual)
+
+    assertEquals(
+      actual,
+      s"""
+      |struct Light{
+      |  vec3 eyePosOrDir;
+      |  bool isDirectional;
+      |  vec3 intensity;
+      |  float attenuation;
+      |};
+      |Light makeLight(){
+      |  return Light(vec3(1.0),true,vec3(2.0),2.5);
+      |}
+      |void frag(){
+      |  Light x=makeLight();
+      |  float y=x.eyePosOrDir.y;
+      |}
+      |""".stripMargin.trim
+    )
+  }
+
 }
 
 object Importable:
