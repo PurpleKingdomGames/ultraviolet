911bfa6b7195e2153fa91c1a655d669ee8546caf
Dave Smith <davesmith.00000@gmail.com>
2022-12-30 15:55:10 +0000

    Shader is an opaque type (again)

--- ultraviolet/shared/src/main/scala/ultraviolet/datatypes/Shader.scala
+++ ultraviolet/shared/src/main/scala/ultraviolet/datatypes/Shader.scala
@@ -6,20 +6,12 @@ import scala.deriving.Mirror
 
 /** A `Shader` is a program that can be run on a graphics card as part of the rendering pipeline.
   */
-final case class Shader[In, Out](body: In => Out):
-  /** `run` is useful for testing shaders as it allows you to treat the shader like an ordinary function call.
-    */
-  def run(in: In): Out = body(in)
+opaque type Shader[In, Out] = In => Out
 
 object Shader:
-  inline def apply[In, Out](f: In => Out): Shader[In, Out] =
-    new Shader[In, Out](f)
-
-  inline def apply[In](f: In => Unit): Shader[In, Unit] =
-    new Shader[In, Unit](f)
-
-  inline def apply(body: => Any): Shader[Unit, Unit] =
-    new Shader[Unit, Unit]((_: Unit) => body)
+  inline def apply[In, Out](f: In => Out): Shader[In, Out] = f
+  inline def apply[In](f: In => Unit): Shader[In, Unit]    = f
+  inline def apply(body: => Any): Shader[Unit, Unit]       = (_: Unit) => body
 
   /** `fromFile` allows you to load raw GLSL code from a file at compile time to produce a shader.
     */
@@ -29,5 +21,5 @@ object Shader:
     }
 
   extension [In, Out](inline ctx: Shader[In, Out])
-    inline def toGLSL[T](using ShaderPrinter[T]): ShaderOutput =
-      ShaderMacros.toAST(ctx).render
+    inline def toGLSL[T](using ShaderPrinter[T]): ShaderOutput = ShaderMacros.toAST(ctx).render
+    inline def run(in: In): Out                                = ctx(in)
