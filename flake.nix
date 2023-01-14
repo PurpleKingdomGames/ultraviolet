4e209517ba9bc04bae03a3d1396f2f5242580c7d
Dave Smith <davesmith.00000@gmail.com>
2023-01-14 22:04:59 +0000

    Replace Nix Shell with Nix Flakes

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        jdkToUse = pkgs.jdk17;
+        sbtWithJRE = pkgs.sbt.override { jre = jdkToUse; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            jdkToUse
+            sbtWithJRE
+          ];
+        };
+      }
+    );
+}
