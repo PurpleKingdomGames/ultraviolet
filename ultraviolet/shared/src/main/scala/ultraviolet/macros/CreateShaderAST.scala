c1ad8a715d671c3d42e3829221af339d11291621
Dave Smith <davesmith.00000@gmail.com>
2024-01-02 12:49:27 +0000

    Add support for missing assignment ops + tests

--- ultraviolet/shared/src/main/scala/ultraviolet/macros/CreateShaderAST.scala
+++ ultraviolet/shared/src/main/scala/ultraviolet/macros/CreateShaderAST.scala
@@ -1205,6 +1205,13 @@ class CreateShaderAST[Q <: Quotes](using val qq: Q) extends ShaderMacroUtils:
                 rt
               )
 
+          case "<<" | ">>" | "&" | "^" | "|" =>
+            // Bitwise ops
+            val lhs = walkTerm(term, envVarName)
+            val rhs = xs.headOption.map(tt => walkTerm(tt, envVarName)).getOrElse(ShaderAST.Empty())
+            val rt  = findReturnType(lhs)
+            ShaderAST.Infix(op, lhs, rhs, rt)
+
           case _ =>
             throw ShaderError.Unsupported("Shaders do not support infix operator: " + op)
 
@@ -1230,6 +1237,13 @@ class CreateShaderAST[Q <: Quotes](using val qq: Q) extends ShaderMacroUtils:
                 List(lhs, rhs),
                 rt
               )
+
+          case "<<" | ">>" | "&" | "^" | "|" =>
+            // Bitwise ops
+            val lhs = walkTerm(l, envVarName)
+            val rhs = walkTerm(r, envVarName)
+            val rt  = findReturnType(lhs)
+            ShaderAST.Infix(op, lhs, rhs, rt)
 
           case _ =>
             throw ShaderError.Unsupported("Shaders do not support infix operator: " + op)
