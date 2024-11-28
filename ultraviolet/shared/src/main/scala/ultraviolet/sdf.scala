b0390940f93214df00e02bbd8a85632b3cd44518
davesmith00000 <davesmith.00000@gmail.com>
2024-11-28 22:11:26 +0000

    Fixed #78: Add SDF helpers

---
+++ ultraviolet/shared/src/main/scala/ultraviolet/sdf.scala
@@ -0,0 +1,26 @@
+package ultraviolet
+
+import ultraviolet.syntax.*
+
+object sdf:
+
+  inline def circle(point: vec2, radius: Float): Float =
+    length(point) - radius
+
+  inline def square(point: vec2, halfSize: vec2): Float =
+    val d = abs(point) - halfSize
+    length(max(d, 0.0f)) + min(max(d.x, d.y), 0.0f)
+
+  inline def star(point: vec2, radius: Float, innerRadius: Float): Float =
+    val k1: vec2 = vec2(0.809016994375f, -0.587785252292f)
+    val k2: vec2 = vec2(-k1.x, k1.y)
+    var p2       = vec2(abs(point.x), point.y)
+
+    p2 = p2 - 2.0f * max(dot(k1, p2), 0.0f) * k1
+    p2 = p2 - 2.0f * max(dot(k2, p2), 0.0f) * k2
+    p2 = vec2(abs(p2.x), p2.y - radius)
+
+    val ba: vec2 = innerRadius * vec2(-k1.y, k1.x) - vec2(0.0f, 1.0f)
+    val h: Float = clamp(dot(p2, ba) / dot(ba, ba), 0.0f, radius)
+
+    length(p2 - ba * h) * sign(p2.y * ba.x - p2.x * ba.y)
